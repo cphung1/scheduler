49135f66d04d73416c86e2736f3dc1ed41497222
cphung1 <cphung97@gmail.com>
2020-03-17 16:58:59 -0700

    added skeleton for Appointment folder

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,21 +4,24 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
   console.log(props.interviewers)
+
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={event => props.onChange(interviewer.id)}
+      />
+    );
+  });
+
   return (
   <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
     <ul className="interviewers__list">
-    { props.interviewers.map(element => {
-        return (
-          <InterviewerListItem 
-          key={element.id}
-          name={element.name} 
-          avatar={element.avatar} 
-          selected={element.id === props.interviewer}
-          setInterviewer= {event => props.setInterviewer(element.id)}
-          />
-        );
-      }) }
+    { interviewers }
     </ul>
   </section>
   );
