08f23a82c3efc4450f31883a3cf8367d768e24cd
cphung1 <cphung97@gmail.com>
2020-03-24 16:23:28 -0700

    added test to appointment, 'xit' the tests that simply 'renders without crashing'

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,18 @@
 import React from "react";
 
-import { render, cleanup, getByText, waitForElement, fireEvent } from "@testing-library/react";
+import { 
+  render, 
+  cleanup, 
+  waitForElement, 
+  fireEvent, 
+  getByText, 
+  prettyDOM, 
+  wait, 
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+  queryByText,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -8,12 +20,45 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
+describe ("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
+  
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
 
-  await waitForElement(() => getByText("Monday"));
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = getAllByTestId(container, "appointment")[0]; 
 
-  fireEvent.click(getByText("Tuesday"));
+    fireEvent.click(getByAltText(appointment, "Add"));
 
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+      );
+      
+      expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+
+})
--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -6,7 +6,7 @@ import DayListItem from "components/DayListItem";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
+xit("renders without crashing", () => {
   render(<DayListItem />);
 });
 
@@ -23,4 +23,21 @@ it("renders '1 spot remaining' when there is 1 spot", () => {
 it("renders '2 spots remaining' when there are 2 spots", () => {
   const { getByText } = render(<DayListItem name="Monday" spots={2} />);
   expect(getByText("2 spots remaining")).toBeInTheDocument();
+});
+
+
+it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  // 1. Render the Application.
+  const { container } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  // 3. Click the "Add" button on the first empty appointment.
+  // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+  // 5. Click the first interviewer in the list.
+  // 6. Click the "Save" button on that same appointment.
+  // 7. Check that the element with the text "Saving" is displayed.
+  // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
+  // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
 });
