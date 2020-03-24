c2c5eebd720fc856f725df8cf0b2e7c05ce6f5f6
cphung1 <cphung97@gmail.com>
2020-03-24 14:44:14 -0700

    creat mock module for axios, added test for Application.test.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -19,9 +19,14 @@ export default function useApplicationData() {
   };
 
   useEffect(() => {
-    const promiseDays = axios.get('http://localhost:8001/api/days');
-    const promiseAppts = axios.get('http://localhost:8001/api/appointments');
-    const promiseIntervierwers = axios.get('http://localhost:8001/api/interviewers');
+    // const promiseDays = axios.get('http://localhost:8001/api/days');
+    // const promiseAppts = axios.get('http://localhost:8001/api/appointments');
+    // const promiseIntervierwers = axios.get('http://localhost:8001/api/interviewers');
+    
+    const promiseDays = axios.get('/api/days');
+    const promiseAppts = axios.get('/api/appointments');
+    const promiseIntervierwers = axios.get('/api/interviewers');
+    
     Promise.all([promiseDays, promiseAppts, promiseIntervierwers])
       .then((all) => {
         setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
